6d86f988dc5e9cd7278e7488b9c5277f2c055578
Abderrahmen Dai <abderrahmen.dai.11@gmail.com>
2023-12-05 14:01:30 +0100

    set up of the google and github auth

---
+++ app/ClientMember/page.jsx
@@ -0,0 +1,8 @@
+"use client"
+const Member = () => {
+  return (
+    <div>Member Client Session</div>
+  )
+}
+
+export default Member
---
+++ app/api/auth/[...nextauth]/option.js
@@ -0,0 +1,50 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+    provider: [
+        GithubProvider({
+            profile(profile) {
+                console.log("Profile GitHub: ", profile);
+
+                let userRole = "GitHub User";
+                if (profile?.email == "abderrahmen.dai.11@gmail.com") {
+                    userRole = "admin";
+                }
+
+                return {
+                    ...profile,
+                    role: userRole,
+                };
+            },
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_Secret,
+        }),
+
+        GoogleProvider({
+            profile(profile) {
+                console.log("Profile Google: ", profile);
+
+                let userRole = "Google User";
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: userRole,
+                };
+            },
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_Secret,
+        }),
+    ],
+    
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) token.role = user.role;
+            return token;
+        },
+        async session({ session, token }) {
+            if (session?.user) session.user.role = token.role;
+            return session;
+        },
+    }
+}
